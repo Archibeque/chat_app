42065d4176b8068fdfa1f96bb6dc948f7a45140b
nnadidan360 <nnadidan360@gmail.com>
2022-09-03 02:28:31 +0100

    made changes

--- server.js
+++ server.js
@@ -64,10 +64,10 @@ app.use('', require('./routes/Friend'))
 
 
 //dbconfig
-// const connection_url = 'mongodb+srv://dblord:ready007@cluster0.xr77h.mongodb.net/chatbackenddb?retryWrites=true&w=majority'
+const connection_url = 'mongodb+srv://nnadidblord:badboyz912@cluster0.xr77h.mongodb.net/chatbackend?retryWrites=true&w=majority'
 // const connection_url = 'mongodb://localhost:27017/emmanuel'
 
-const dba =  mongoose.connect(process.env.MONGO_URI, {
+const dba =  mongoose.connect(connection_url, {
     useNewUrlParser: true, 
     // useUnifiedTopology: true, 
     // useCreateIndex: true,
